949f15825233b8c1ed50fd1b70657e6e5da1b8ae
henry <hgbarrow@gmail.com>
2019-07-07 11:14:11 -0700

    Update packages and 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404</h1>
+    <h1>
+      Page not found{' '}
+      <span role="img" aria-label="sad face">
+        😰
+      </span>
+    </h1>
+    <h3>
+      Go <Link to="/">home</Link>
+    </h3>
   </Layout>
 )
 
